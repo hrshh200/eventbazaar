c7fbf8f2ae4617a37bad720741019a25b9c488ac
Harsh <hsharmarishra@gmail.com>
2024-10-13 23:48:55 +0530

    Created pages for Eventbazaar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className='container'>
+            <div className='headingDisplay'>
+                <img
+                    className='logoDisplay'
+                    src='Logo.png' />
+                <span className='headingTitleWed'>
+                    Events and Weddings
+                </span>
+            </div>
+            <div className='displayList'>
+                <ul>
+                    <li>Home</li>
+                    <li>Sevices</li>
+                    <li>About</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+                <div className='detailsList'>
+                    <span>+91 88866668888</span>
+                    <span>info@kingsevents.com</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
